5a92c4f5812a44aedfe43eb7705bbe64a7d6aa12
yoavain <yoavain@2a676a6f-2e4a-042c-16da-833eb8755bbc>
2014-10-24 10:30:45 +0000

    Removed bin folder from repository.

--- trunk/FanartHandler/bin/Release/merge.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-IF EXIST FanartHandler_UNMERGED.dll del FanartHandler_UNMERGED.dll
-ren FanartHandler.dll FanartHandler_UNMERGED.dll
-ilmerge /out:FanartHandler.dll FanartHandler_UNMERGED.dll NLog.dll
