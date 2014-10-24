b64dd78b4765d0eda11dc8c9ecdf01c78c599a47
yoavain <yoavain@2a676a6f-2e4a-042c-16da-833eb8755bbc>
2014-10-24 10:07:47 +0000

    Initial scripts and stuff

---
+++ trunk/scripts/Build.bat
@@ -0,0 +1,20 @@
+@echo off
+cls
+Title Building MediaPortal Fanart Handler (RELEASE)
+cd ..
+
+if "%programfiles(x86)%XXX"=="XXX" goto 32BIT
+	:: 64-bit
+	set PROGS=%programfiles(x86)%
+	goto CONT
+:32BIT
+	set PROGS=%ProgramFiles%	
+:CONT
+
+:: Build
+"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" /target:Rebuild /property:Configuration=RELEASE /fl /flp:logfile=FanartHandler.log;verbosity=diagnostic FanartHandler.sln
+
+cd scripts
+
+pause
+
