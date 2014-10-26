dbf5dc2a664d61e41a03b17511fbd01b67ffc30d
yoavain <yoavain@2a676a6f-2e4a-042c-16da-833eb8755bbc>
2014-10-26 12:10:54 +0000

    Merge Nlog.dll into FanartHandler.dll

---
+++ scripts/merge.bat
@@ -0,0 +1,13 @@
+@echo off
+
+if "%programfiles(x86)%XXX"=="XXX" goto 32BIT
+    :: 64-bit
+    set PROGS=%programfiles(x86)%
+    goto CONT
+:32BIT
+    set PROGS=%ProgramFiles%
+:CONT
+
+if exist FanartHandler_UNMERGED.dll del FanartHandler_UNMERGED.dll
+ren FanartHandler.dll FanartHandler_UNMERGED.dll 
+ilmerge.exe /out:FanartHandler.dll FanartHandler_UNMERGED.dll Nlog.dll /target:dll /targetplatform:"v4,%PROGS%\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.0" /wildcards
