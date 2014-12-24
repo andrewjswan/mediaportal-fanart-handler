efff9975e198de60042703cb15b65959e5a7cf71
ajs.blackhole@gmail.com <ajs.blackhole@gmail.com@2a676a6f-2e4a-042c-16da-833eb8755bbc>
2014-12-24 12:17:01 +0000

    # change bat file for my system (but rem my path)

--- scripts/Create_Installer.bat
+++ scripts/Create_Installer.bat
@@ -5,19 +5,19 @@ Title Creating MediaPortal FanartHandler Installer
 :: Check for modification
 svn status ..\source | findstr "^M"
 if ERRORLEVEL 1 (
-	echo No modifications in source folder.
+    echo No modifications in source folder.
 ) else (
-	echo There are modifications in source folder. Aborting.
-	pause
-	exit 1
+    echo There are modifications in source folder. Aborting.
+    pause
+    exit 1
 )
 
 if "%programfiles(x86)%XXX"=="XXX" goto 32BIT
-	:: 64-bit
-	set PROGS=%programfiles(x86)%
-	goto CONT
+    :: 64-bit
+    set PROGS=%programfiles(x86)%
+    goto CONT
 :32BIT
-	set PROGS=%ProgramFiles%	
+    set PROGS=%ProgramFiles%    
 :CONT
 
 :: Get version from DLL
@@ -34,23 +34,24 @@ Tools\sed.exe -i "s/fanarthandler-{VERSION}.xml/fanarthandler-%version%.xml/g" f
 
 :: Build MPE1
 "%PROGS%\Team MediaPortal\MediaPortal\MPEMaker.exe" fanarthandlerTemp.xmp2 /B /V=%version% /UpdateXML
+REM "D:\MediaPortal\MPEMaker.exe" fanarthandlerTemp.xmp2 /B /V=%version% /UpdateXML
 
 :: Cleanup
 del fanarthandlerTemp.xmp2
 
-:: Sed "FanartHandler-{VERSION}.MPE1" from fanarthandler.xml
-Tools\sed.exe -i "s/FanartHandler-{VERSION}.MPE1/FanartHandler-%version%.MPE1/g" fanarthandler-%version%.xml
+:: Sed "FanartHandler-{VERSION}.mpe1" from fanarthandler.xml
+Tools\sed.exe -i "s/FanartHandler-{VERSION}.mpe1/FanartHandler-%version%.mpe1/g" fanarthandler-%version%.xml
 
 :: Parse version (Might be needed in the futute)
 FOR /F "tokens=1-4 delims=." %%i IN ("%version%") DO ( 
-	SET major=%%i
-	SET minor=%%j
-	SET build=%%k
-	SET revision=%%l
+    SET major=%%i
+    SET minor=%%j
+    SET build=%%k
+    SET revision=%%l
 )
 
 :: Rename MPE1
-if exist "..\builds\FanartHandler-%major%.%minor%.%build%.%revision%.MPE1" del "..\builds\FanartHandler-%major%.%minor%.%build%.%revision%.MPE1"
-rename ..\builds\FanartHandler-MAJOR.MINOR.BUILD.REVISION.MPE1 "FanartHandler-%major%.%minor%.%build%.%revision%.MPE1"
+if exist "..\builds\FanartHandler-%major%.%minor%.%build%.%revision%.mpe1" del "..\builds\FanartHandler-%major%.%minor%.%build%.%revision%.mpe1"
+rename ..\builds\FanartHandler-MAJOR.MINOR.BUILD.REVISION.mpe1 "FanartHandler-%major%.%minor%.%build%.%revision%.mpe1"
 
 
