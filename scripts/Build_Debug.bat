9244073f55bba0f4ee3ec4c99d4018e632c6f014
yoavain <yoavain@gmail.com>
2015-03-24 20:44:24 +0200

    Fix scripts for GIT

--- scripts/Build_Debug.bat
+++ scripts/Build_Debug.bat
@@ -3,6 +3,8 @@ cls
 Title Building MediaPortal Fanart Handler (RELEASE)
 cd ..
 
+setlocal enabledelayedexpansion
+
 if "%programfiles(x86)%XXX"=="XXX" goto 32BIT
 	:: 64-bit
 	set PROGS=%programfiles(x86)%
@@ -11,8 +13,16 @@ if "%programfiles(x86)%XXX"=="XXX" goto 32BIT
 	set PROGS=%ProgramFiles%	
 :CONT
 
+: Prepare version
+for /f "tokens=*" %%a in ('git rev-list HEAD --count') do set REVISION=%%a 
+set REVISION=%REVISION: =%
+"scripts\Tools\sed.exe" -i "s/\$WCREV\$/%REVISION%/g" FanartHandler\Properties\AssemblyInfo.cs
+
 :: Build
 "%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" /target:Rebuild /property:Configuration=DEBUG /fl /flp:logfile=FanartHandler.log;verbosity=diagnostic FanartHandler.sln
+
+:: Revert version
+git checkout FanartHandler\Properties\AssemblyInfo.cs
 
 cd scripts
 
