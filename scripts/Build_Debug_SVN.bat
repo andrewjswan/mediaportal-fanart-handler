126cfe7c21a569069fc9a5d2d4172f8483a513f7
yoavain <yoavain@gmail.com>
2015-03-24 22:15:55 +0200

    Script for both GIT & SVN

--- scripts/Build_Debug_SVN.bat
+++ scripts/Build_Debug_SVN.bat
@@ -3,8 +3,6 @@ cls
 Title Building MediaPortal Fanart Handler (RELEASE)
 cd ..
 
-setlocal enabledelayedexpansion
-
 if "%programfiles(x86)%XXX"=="XXX" goto 32BIT
 	:: 64-bit
 	set PROGS=%programfiles(x86)%
@@ -14,15 +12,13 @@ if "%programfiles(x86)%XXX"=="XXX" goto 32BIT
 :CONT
 
 : Prepare version
-for /f "tokens=*" %%a in ('git rev-list HEAD --count') do set REVISION=%%a 
-set REVISION=%REVISION: =%
-"scripts\Tools\sed.exe" -i "s/\$WCREV\$/%REVISION%/g" FanartHandler\Properties\AssemblyInfo.cs
+subwcrev . FanartHandler\Properties\AssemblyInfo.cs FanartHandler\Properties\AssemblyInfo.cs
 
 :: Build
 "%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" /target:Rebuild /property:Configuration=DEBUG /fl /flp:logfile=FanartHandler.log;verbosity=diagnostic FanartHandler.sln
 
 :: Revert version
-git checkout FanartHandler\Properties\AssemblyInfo.cs
+svn revert FanartHandler\Properties\AssemblyInfo.cs
 
 cd scripts
 
---
+++ scripts/Build_GIT.bat
@@ -0,0 +1,30 @@
+@echo off
+cls
+Title Building MediaPortal Fanart Handler (RELEASE)
+cd ..
+
+setlocal enabledelayedexpansion
+
+if "%programfiles(x86)%XXX"=="XXX" goto 32BIT
+	:: 64-bit
+	set PROGS=%programfiles(x86)%
+	goto CONT
+:32BIT
+	set PROGS=%ProgramFiles%	
+:CONT
+
+: Prepare version
+for /f "tokens=*" %%a in ('git rev-list HEAD --count') do set REVISION=%%a 
+set REVISION=%REVISION: =%
+"scripts\Tools\sed.exe" -i "s/\$WCREV\$/%REVISION%/g" FanartHandler\Properties\AssemblyInfo.cs
+	
+:: Build
+"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" /target:Rebuild /property:Configuration=RELEASE /fl /flp:logfile=FanartHandler.log;verbosity=diagnostic FanartHandler.sln
+
+: Revert version
+git checkout FanartHandler\Properties\AssemblyInfo.cs
+
+cd scripts
+
+pause
+
