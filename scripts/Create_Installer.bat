f77e72e6320ad89a3e6eff09c58acd2c6917f2d8
Andrew J.Swan <ajs.blackhole@gmail.com>
2024-03-27 17:11:03 +0200

    Update Create Installer

--- scripts/Create_Installer.bat
+++ scripts/Create_Installer.bat
@@ -15,5 +15,12 @@ IF NOT EXIST "%PROGS%\Team MediaPortal\MediaPortal\" SET PROGS=C:
 :: Get version from DLL
 FOR /F "tokens=*" %%i IN ('Tools\sigcheck.exe /accepteula /nobanner /n "..\FanartHandler\bin\Release\FanartHandler.dll"') DO (SET version=%%i)
 
+:: Temp xmp2 file
+copy /Y fanarthandler.xmp2 fanarthandlertemp.xmp2
+
 :: Build MPE1
-"%PROGS%\Team MediaPortal\MediaPortal\MPEMaker.exe" fanarthandler.xmp2 /B /V=%version% /UpdateXML
+"%PROGS%\Team MediaPortal\MediaPortal\MPEMaker.exe" fanarthandlertemp.xmp2 /B /V=%version% /UpdateXML
+
+:: Cleanup
+del fanarthandlertemp.xmp2
+
