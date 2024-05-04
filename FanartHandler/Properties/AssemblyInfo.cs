efa61b9cf5d52ed1eb23212f410b7a5c3387d2bd
Andrew J.Swan <ajs.blackhole@gmail.com>
2024-05-04 13:55:32 +0300

    Change version numeration

--- FanartHandler/Properties/AssemblyInfo.cs
+++ FanartHandler/Properties/AssemblyInfo.cs
@@ -42,5 +42,5 @@ using MediaPortal.Common.Utils;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.5.$WCREV$")]
-[assembly: AssemblyFileVersion("4.0.5.$WCREV$")]
+[assembly: AssemblyVersion("4.0.5.999")]
+[assembly: AssemblyFileVersion("4.0.5.999")]
--- scripts/Build_GIT.bat
+++ scripts/Build_GIT.bat
@@ -17,7 +17,7 @@ IF NOT EXIST "%PROGS%\Team MediaPortal\MediaPortal\" SET PROGS=C:
 : Prepare version
 for /f "tokens=*" %%a in ('git rev-list HEAD --count') do set REVISION=%%a 
 set REVISION=%REVISION: =%
-"scripts\Tools\sed.exe" -i "s/\$WCREV\$/%REVISION%/g" FanartHandler\Properties\AssemblyInfo.cs
+"scripts\Tools\sed.exe" -i -r "s/(Assembly(File)?Version\(.[0-9]+\.[0-9]+\.[0-9]+\.)[0-9]+(.\))/\1%REVISION%\3/g" FanartHandler\Properties\AssemblyInfo.cs
 	
 :: Build
 "%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" /target:Rebuild /property:Configuration=RELEASE /fl /flp:logfile=FanartHandler.log;verbosity=diagnostic FanartHandler.sln
